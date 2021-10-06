c34ad425295ab12f2f566d787ec345d800083e6e
Olekasandr <74654117+Alex1120001@users.noreply.github.com>
2021-10-06 15:19:30 +0300

    Add files via upload

---
+++ script/js.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById('btn-form');
+
+btn.onclick = function(){
+    alert('Дякуємо за замовлення!\n Очікуйте!');
+    window.location.href = 'https://www.google.com/?client=safari&channel=mac_bm.ua/';
+
+}
